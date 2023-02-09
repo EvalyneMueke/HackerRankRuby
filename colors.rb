b8e77f3bc8dfe8fecfd5e26182b62d09dba1dc26
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-02-09 12:37:59 +0300

    Solved second challenge

---
+++ colors.rb
@@ -0,0 +1,10 @@
+def iterate_colors(colors)
+    result = []
+    colors.each do |color|
+      result << color
+    end
+    result
+  end
+
+  #to print the content in the vs code terminal
+  puts (["green","red"])
